697bda9d5075bbec3712e8f9686c90c5f9e639fb
nzi19001@byui.edu <nzi19001@byui.edu>
2023-12-22 18:25:28 +0300

    created h2 element in script

---
+++ script.js
@@ -0,0 +1,6 @@
+//create h2 element and put some text
+
+const h2 = document.createElement("h2");
+h2.textContent = "This content added by JavaScript";
+//add h2 to body
+document.querySelector("body").appendChild(h2)
